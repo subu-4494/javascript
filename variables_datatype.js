24c11a94355d8d6e3a9313a810a251faa4609fd9
Sourabh <sourabroy2018@gmail.com>
2024-12-09 17:24:47 +0530

    datatypes

---
+++ variables_datatype.js
@@ -0,0 +1,33 @@
+// var age =23;
+// if(true)
+// {
+//     console.log(age);
+// }
+
+// function solve()
+// {
+//     var age = 45;
+//     console.log(age);
+// }
+// console.log(age);
+// solve();
+{
+let a =10;
+console.log(a);
+}
+a=98;    // dynamically type language
+a = "subu";// changes its value to string 
+console.log(a);
+
+const v= 98;
+console.log(v);
+
+let r;                  // undefined
+console.log(r);
+
+
+let f= null;                  // null data type
+console.log(f);
+
+let datatypes= false;
+console.log(typeof(datatypes));   // type of datatypes
